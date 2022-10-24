b736e8be52904267d18217b42f9793713cdfdfdf
Martin <escorpmartin97@gmail.com>
2022-10-24 16:30:56 -0300

    styles: importa tema da app e adiciona estilos globais

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,7 +3,14 @@ import { createStitches } from '@stitches/react'
 export const { config, styled, globalCss, css, keyframes, getCssText, theme, createTheme } = createStitches({
   theme: {
     colors: {
-      rocket: '#8257E6'
+      white: '#fff',
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e'
     }
   }
 })
