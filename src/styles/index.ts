c8e680785a8c12be4947cf4e3d500a0011e3ab15
Martin <escorpmartin97@gmail.com>
2022-10-24 16:02:11 -0300

    feat: carrega css em cache

---
+++ src/styles/index.ts
@@ -0,0 +1,9 @@
+import { createStitches } from '@stitches/react'
+
+export const { config, styled, globalCss, css, keyframes, getCssText, theme, createTheme } = createStitches({
+  theme: {
+    colors: {
+      rocket: '#8257E6'
+    }
+  }
+})
