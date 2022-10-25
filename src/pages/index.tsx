fe5b73b99a2c558e25a3aec91b1140cf7da83a2b
Martin <escorpmartin97@gmail.com>
2022-10-25 12:01:44 -0300

    feat: adiciona carrossel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,45 @@
 import Image from "next/image"
 import { HomeContainer, Product } from "../styles/pages/home"
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 import c1 from '../assets/camisetas/c1.png'
 import c2 from '../assets/camisetas/c2.png'
 
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={c1} alt="" width={520} height={480} />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={c2} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={c2} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
         <Image src={c2} alt="" width={520} height={480} />
         <footer>
           <strong>Camiseta X</strong>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -12,5 +12,5 @@ export const Header = styled('header', {
   padding: '2rem 0',
   width: '100%',
   maxWidth:1180,
-  margin: '0, auto'
+  margin: '0 auto'
 })
