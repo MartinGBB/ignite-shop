a23594b7dedd35728fec8df272858333dc291172
Martin <escorpmartin97@gmail.com>
2022-10-26 15:37:58 -0300

    styles: estrutura do componente de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta</h1>
+        <span>R$ 79,99</span>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident laborum praesentium facere quia nostrum ratione quibusdam repellendus veritatis cum odio blanditiis in, at tempora corporis, illum asperiores aut ipsam doloremque.</p>
+      <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
---
+++ src/styles/pages/product.ts
@@ -0,0 +1,68 @@
+import { styled } from "..";
+
+export const ProductContainer = styled('main', {
+  display: "grid",
+  gridTemplateColumns: 'repeat(2, 1fr)',
+  alignItems: 'stretch',
+  gap: '4rem',
+  margin: '0 auto',
+  maxWidth: 1180
+})
+
+export const ImageContainer = styled('main', {
+  width: '100%',
+  maxWidth: 576,
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  height: '656px',
+
+  display: "flex",
+  alignItems: 'center',
+  justifyContent: "center",
+
+  img: {
+    objectFit: 'cover',
+  }
+})
+
+export const ProductDetails = styled('main', {
+  display: "flex",
+  flexDirection: 'column',
+  
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray300',
+  },
+
+  span: {
+    marginTop: '1rem',
+    display: 'block',
+    fontSize: '$2xl',
+    color: '$green300',
+
+  },
+
+  p: {
+    marginTop: '2.5rem',
+    fontSize: '$md',
+    lineHeight: '1.6',
+    color: '$gray-300',
+  },
+
+  button: {
+    marginTop: 'auto',
+    background: '$green500',
+    border: 0,
+    color: '$white',
+    borderRadius: 8,
+    padding: '1.25rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontSize: '$md',
+    
+    '&:hover': {
+      background: '$green300',
+      transition: '1s',
+    }
+  },
+})
