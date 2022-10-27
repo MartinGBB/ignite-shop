5f1500fa4fabbe81d06d986d63551bd7324dfd4a
Martin <escorpmartin97@gmail.com>
2022-10-27 17:05:28 -0300

    feat: adiciona prefetch para apenas precarregar com o hover em produção

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home({ products }: HomeProps) {
       
       {products.map((product) => {
         return (
-          <Link href={`/product/${product.id}`} key={product.id}>
+          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
             <Product className="keen-slider__slide">
               <Image placeholder="blur" blurDataURL={product.imageUrl} src={product.imageUrl} alt="" width={520} height={480} />
               <footer>
