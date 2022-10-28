6b97b8ed15b59faaab805ee95d0d85b5eddd4a8d
Martin <escorpmartin97@gmail.com>
2022-10-28 09:49:04 -0300

    feat: redireciona usuario ao clicar em comprar

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -2,7 +2,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../lib/stripe";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const priceId = 'price_1Lwqt7BWNdAduV2MBjCso0yQ'
+  const { priceId } = req.body
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price not found.' })
+  }
+console.log(req.method)
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed.' })
+  }
 
   const successUrl = `${process.env.NEXT_URL}/`
   const cancelUrl = `${process.env.NEXT_URL}/`
@@ -17,7 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         quantity: 1,
       }
     ],
-  })
+})
 
   return res.status(201).json({
     checkoutUrl: checkoutSession.url,
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,8 @@
+import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
@@ -18,10 +20,24 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
   const { isFallback } = useRouter()
 
-  function handleClick() {
-    console.log(product.defaultPriceId)
+  async function handleClick() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      setIsCreatingCheckoutSession(false)
+      alert('falha ao redirecionar')
+    }
   }
 
   if (isFallback) {
@@ -38,7 +54,7 @@ export default function Product({ product }: ProductProps) {
         <span>{product.price}</span>
 
         <p>{product.description}</p>
-      <button onClick={handleClick}>Comprar agora</button>
+      <button disabled={isCreatingCheckoutSession} onClick={handleClick}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
