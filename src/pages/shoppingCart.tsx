0d1fcdfeaa3f82fb8ca74f70f3cd0bfca6a6a556
Martin <escorpmartin97@gmail.com>
2022-10-31 17:47:38 -0300

    feat: estrutura dos produtos no cart

--- src/pages/shoppingCart.tsx
+++ src/pages/shoppingCart.tsx
@@ -1,9 +1,23 @@
-import { ShoppingCartContainer } from "../styles/pages/ShoppingCart";
+import { X } from "phosphor-react";
+import { ImageContent, Products, ShoppingCartContainer } from "../styles/pages/ShoppingCart";
 
 export default function ShoppingCart() {
   return (
     <ShoppingCartContainer>
-      <h1>Carrinho de compras</h1>
+      <span><X size={24} color="#8D8D99" weight="bold" /> </span>
+      <h1>Sacola de compras</h1>
+
+      <Products>
+        <ImageContent>
+
+        </ImageContent>
+
+        <div>
+          <h2>Camiseta Beyond the Limits</h2>
+          <strong>R$ 79,90</strong>
+          <button>Remover</button>
+        </div>
+      </Products>
     </ShoppingCartContainer>
   )
 }
--- src/styles/pages/ShoppingCart.ts
+++ src/styles/pages/ShoppingCart.ts
@@ -9,5 +9,57 @@ export const ShoppingCartContainer = styled('div', {
   height: '100vh',
   maxWidth: '30rem',
   padding: '3rem',
-  paddingTop: '4.5rem'
+  paddingTop: '4.5rem',
+
+  span: {
+    display: 'flex',
+    justifyContent: 'end',
+    lineHeight: 0,
+  },
+
+  h1 : {
+    margin: '1.25rem 0 2rem',
+    fontSize: "1.25rem"
+  },
+})
+
+export const Products = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  gap: '1.25rem',
+  
+  div: {
+    display: 'flex',
+    flexDirection: 'column',
+    lineHeight: '160%',
+
+    h2: {
+      fontSize: '1.125rem',
+      fontWeight: '400',
+    },
+
+    strong: {
+      fontSize: '1.125rem',
+    },
+  
+    button: {
+      border: 'none',
+      background: 'transparent',
+      fontWeight: 'bold',
+      color: '$green500',
+
+      display: 'flex',
+      marginTop: 'auto',
+      fontSize: '1rem',
+
+      cursor: 'pointer', 
+    }
+  }
+})
+
+export const ImageContent = styled('div', {
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  width: '6.37rem',
+  height: '6.37rem',
+  borderRadius: '8px',
 })
