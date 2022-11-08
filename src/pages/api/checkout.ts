08e6d6fa493fc525f9700249e7ad46717b69a848
Martin <escorpmartin97@gmail.com>
2022-11-08 19:56:44 -0300

    feat: teste de correção de erro en variaveis

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -7,8 +7,6 @@ export default async function handler(
 ) {
   const { products } = req.body
 
-  const { VERCEL_URL, NEXT_URL } = process.env
-
   if (!products) {
     return res.status(400).json({ error: 'Price not found.' })
   }
@@ -17,9 +15,7 @@ export default async function handler(
     return res.status(405).json({ error: 'Method not allowed.' })
   }
 
-  const successUrl = `${
-    VERCEL_URL || NEXT_URL
-  }/success?session_id={CHECKOUT_SESSION_ID}`
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_URL}/`
 
   const arrProducts = Object.keys(products).map((key) => products[key])
